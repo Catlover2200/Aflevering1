5a585ccfa8d712821cd4b6c73218cd48696ddc98
Klara <Klara.blauenfeldt@gmail.com>
2023-02-05 21:43:57 +0100

    små ændringer

--- src/Car.java
+++ src/Car.java
@@ -3,13 +3,10 @@
         public int price;
         public String model;
         public boolean start; {
-
         }
         public boolean stop; {
-
         }
         public boolean move; {
-
         }
     }
 
